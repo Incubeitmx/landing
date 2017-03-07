1ecbbd836d3a186a9261f56be050c5a16de0062e
JeosMac <jeovanymosh@gmail.com>
2017-03-07 13:31:39 -0600

    whithout team or font family

--- src/App.js
+++ src/App.js
@@ -9,39 +9,66 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
+
         <div className="App-header">
           <a href="#"><img src={logoHead} className="head-logo" alt="head-logo"/></a>
           <div className="App-links">
             <p><a href="#App-download" className="links">DESCARGAR</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <a href="#App-why" className="links">¿POR QUÉ DEBERÍA USARLA?</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <a href="#App-about" className="links">ACERCA DE</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="#App-team" className="links">EQUIPO</a></p>
+            </p>
+            <div className="App-intro">
+              <h1>¿Qué es CitiTour Morelia?</h1><br/>
+              <p className="intro">CitiTour Morelia es una aplicación que cuenta todo sobre los sitios de interés histórico, cultural y artísticos más relevantes de la ciudad de Morelia.</p><br/><br/>
+              <div className="btmDownload">
+                <a href="#App-download" className="links2">Descargar App</a>
+              </div>
+            </div>
           </div>
         </div>
+
+
         <div className='App-why' alt="App-why" id="App-why">
           <img src={phone} className="phone-logo" alt="phone-logo"/>
+          <div className="Why-desc">
+            <h1>¿Por qué deberías usarla?</h1><br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel ultricies tellus.
+               Phasellus convallis varius ipsum, nec blandit ipsum pretium id. In hac habitasse platea dictumst. </p>
+             <br/><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ver los eventos más cercanos.</p>
+             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Listado de lugares populares por categoría.</p>
+             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ver lugares y conocer más sobre ellos.</p>
+             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Hacer un recorrido guíado, caminando, en &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bicicleta o en automóvil.</p>
+          </div>
         </div>
+
+
         <div className='App-download' id="App-download">
           <h2>¿Cómo descargarla?</h2>
           <h4>Disponible ya en las tiendas</h4>
           <a href='https://play.google.com/store/apps/details?id=com.cititourIncubeit' className="play-logo"><img src={playstore} className="play-logo" alt="play-logo"/></a>
         </div>
+
+
         <div className='App-about' id="App-about"><br/>
           <h2>Acerca de CitiTour Morelia</h2>
-          <h4>Conóce mas sobre el proyecto.</h4><br/>
-          <br/><p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit Mauris accumsan, orci in ornare feugiat, diam mi venenatis elit, sit ametc<br/>
-          euismod velit mi efficitur ipsum. Nam tristique mi ut vehicula dictum. Aliquam in porttitor nulla. Sed dapibus dignissim metus, vitae<br/>
-          rutrum ex. Proin posuere vitae urna et commodo. Curabitur dignissim luctus sem at tincidunt. Mauris risus arcu, placerat id euismod<br/>
-          non, faucibus in est. Pellentesque pellentesque luctus urna, id porta nisl dignissim eu. Proin eu mattis nulla. Vivamus pellentesque<br/>
-          velit ut nibh maximus tempus. Fusce sed bibendum mi. In rhoncus consequat felis. Vivamus ligula leo, lobortis quis odio non, posuere<br/>
-        interdum justo. Fusce laoreet quam id fermentum varius.</p><br/><br/>
+          <h4>Conóce mas sobre el proyecto.</h4>
+          <p className="descrip"><br/>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit Mauris accumsan, orci in ornare feugiat, diam mi venenatis elit, sit ametc
+          euismod velit mi efficitur ipsum. Nam tristique mi ut vehicula dictum. Aliquam in porttitor nulla. Sed dapibus dignissim metus, vitae
+          rutrum ex. Proin posuere vitae urna et commodo. Curabitur dignissim luctus sem at tincidunt. Mauris risus arcu, placerat id euismod
+          non, faucibus in est. Pellentesque pellentesque luctus urna, id porta nisl dignissim eu. Proin eu mattis nulla. Vivamus pellentesque
+          velit ut nibh maximus tempus. Fusce sed bibendum mi. In rhoncus consequat felis. Vivamus ligula leo, lobortis quis odio non, posuere
+        interdum justo. Fusce laoreet quam id fermentum varius.</p>
         </div>
-        <div className='App-team' id="App-team">
-        </div>
+
+
+
         <div className='App-foot'>
           <img src={logoFoot} className="foot-logo" alt="foot-logo"/>
+          <div className="rights">©2017 CitiTour Morelia.<br/>Todos los derechos reservados.</div>
         </div>
+
       </div>
     );
   }
