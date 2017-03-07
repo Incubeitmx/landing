6434903df80099d60851395fb16ab856858587dd
JeosMac <jeovanymosh@gmail.com>
2017-03-07 11:33:26 -0600

    Node_modules

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logoHead} className="head-logo" alt="head-logo"/>
+
         </div>
         <div className='App-why'>
           <img src={phone} className="phone-logo" alt="phone-logo"/>
@@ -20,7 +21,16 @@ class App extends Component {
           <h4>Disponible ya en las tiendas</h4>
           <a href='https://play.google.com/store/apps/details?id=com.cititourIncubeit' className="play-logo"><img src={playstore} className="play-logo" alt="play-logo"/></a>
         </div>
-        <div className='App-about'>
+        <div className='App-about'><br/>
+          <h2>Acerca de CitiTour Morelia</h2>
+          <h4>Conóce mas sobre el proyecto.</h4><br/>
+          <br/>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit Mauris accumsan, orci in ornare feugiat, diam mi venenatis elit, sit ametc<br/>
+          euismod velit mi efficitur ipsum. Nam tristique mi ut vehicula dictum. Aliquam in porttitor nulla. Sed dapibus dignissim metus, vitae<br/>
+          rutrum ex. Proin posuere vitae urna et commodo. Curabitur dignissim luctus sem at tincidunt. Mauris risus arcu, placerat id euismod<br/>
+          non, faucibus in est. Pellentesque pellentesque luctus urna, id porta nisl dignissim eu. Proin eu mattis nulla. Vivamus pellentesque<br/>
+          velit ut nibh maximus tempus. Fusce sed bibendum mi. In rhoncus consequat felis. Vivamus ligula leo, lobortis quis odio non, posuere<br/>
+        interdum justo. Fusce laoreet quam id fermentum varius.<br/><br/>
         </div>
         <div className='App-team'>
         </div>
