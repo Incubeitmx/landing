5ada8b08a7dd083b358d39e33ed48bda400da9ec
JeosMac <jeovanymosh@gmail.com>
2017-03-07 12:30:07 -0600

    Enlaces

--- src/App.js
+++ src/App.js
@@ -10,29 +10,34 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logoHead} className="head-logo" alt="head-logo"/>
-
+          <a href="#"><img src={logoHead} className="head-logo" alt="head-logo"/></a>
+          <div className="App-links">
+            <p><a href="#App-download" className="links">DESCARGAR</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="#App-why" className="links">¿POR QUÉ DEBERÍA USARLA?</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="#App-about" className="links">ACERCA DE</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="#App-team" className="links">EQUIPO</a></p>
+          </div>
         </div>
-        <div className='App-why'>
+        <div className='App-why' alt="App-why" id="App-why">
           <img src={phone} className="phone-logo" alt="phone-logo"/>
         </div>
-        <div className='App-download'>
+        <div className='App-download' id="App-download">
           <h2>¿Cómo descargarla?</h2>
           <h4>Disponible ya en las tiendas</h4>
           <a href='https://play.google.com/store/apps/details?id=com.cititourIncubeit' className="play-logo"><img src={playstore} className="play-logo" alt="play-logo"/></a>
         </div>
-        <div className='App-about'><br/>
+        <div className='App-about' id="App-about"><br/>
           <h2>Acerca de CitiTour Morelia</h2>
           <h4>Conóce mas sobre el proyecto.</h4><br/>
-          <br/>
+          <br/><p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit Mauris accumsan, orci in ornare feugiat, diam mi venenatis elit, sit ametc<br/>
           euismod velit mi efficitur ipsum. Nam tristique mi ut vehicula dictum. Aliquam in porttitor nulla. Sed dapibus dignissim metus, vitae<br/>
           rutrum ex. Proin posuere vitae urna et commodo. Curabitur dignissim luctus sem at tincidunt. Mauris risus arcu, placerat id euismod<br/>
           non, faucibus in est. Pellentesque pellentesque luctus urna, id porta nisl dignissim eu. Proin eu mattis nulla. Vivamus pellentesque<br/>
           velit ut nibh maximus tempus. Fusce sed bibendum mi. In rhoncus consequat felis. Vivamus ligula leo, lobortis quis odio non, posuere<br/>
-        interdum justo. Fusce laoreet quam id fermentum varius.<br/><br/>
+        interdum justo. Fusce laoreet quam id fermentum varius.</p><br/><br/>
         </div>
-        <div className='App-team'>
+        <div className='App-team' id="App-team">
         </div>
         <div className='App-foot'>
           <img src={logoFoot} className="foot-logo" alt="foot-logo"/>
